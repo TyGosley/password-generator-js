91edd24f2c28c5fa4ef6fceec54420fd574e0dcf
TyGosley <tygosley@gmail.com>
2022-12-27 10:26:18 -0800

    added confirm variables and command

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,11 @@ var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "?", "<", ">", "+", "-", "_", ",", ".", "=", "'"];
 
+var passwordLength;
+var confirmLowerChar;
+var confirmUpperChar;
+var confirmNumbers;
+var confirmSpecialChar;
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -18,35 +23,31 @@ var generateBtn = document.querySelector("#generate");
 // }
 
 // Prompt length of password between 8 and 128: yes or no
-function writePassword(){
-   passwordLength = prompt("How many characters would you like in your password?");
+function generatePassword(){
+  var passwordLength = prompt("How many characters would you like in your password?");
 
-if (passwordLength < 8 || passwordLength > 128) {
+if (passwordLength <= 8 || passwordLength >= 128) {
   alert("Please choose between 8 and 128 characters.");
   passwordLength = prompt("How many characters would you like in your password?");
 }
 };
 
-writePassword();
+generatePassword();
 passwordLength = parseInt(passwordLength);
 
 // confirm prompt
+// Prompt would you like lowerChar
+// Prompt would you like upperChar
+// Prompt would you like numbers
+// Prompt would you like specialChar
 
-// Prompt would you like lowerChar: yes or no
+var confirmLowerChar = confirm("Click ok if you want to include lower case characters in your password.");
+var confirmUpperChar = confirm("Click ok if you want to include upper case characters in your password.");
+var confirmNumbers = confirm("Click ok if you want to include numbers in your password.");
+var confirmSpecialChar = confirm("Click ok if you want to include special characters in your password.");
 
-// confirm prompt
 
-// Prompt would you like upperChar: yes or no
 
-// confirm prompt
-
-// Prompt would you like numbers: yes or no
-
-// confirm prompt
-
-// Prompt would you like specialChar: yes or no
-
-// confirm prompt
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
